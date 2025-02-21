b981798fb435b58fa3a3aef739e1fc59d4b37003
tech-vishal-raina <vishuraina0001@gmail.com>
2025-02-21 19:59:08 +0530

    Time: 0 ms (100%), Space: 23.9 MB (18.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        int cnt = 0;
-        if(n==1) return;
-        for(int i=0; i<n;){
-            if(nums[i]==0){
-               cnt = cnt+1;
-               nums.erase(nums.begin() + i);
-               n--;
-            }
-            else{
-                i++;
-            }
-        }
-        if(cnt==0) return;
-        else{
-            while(cnt!=0){
-                nums.push_back(0);
-                cnt--;
+        // TWO POINTER APPROACH
+        int nonzerotracker =0;
+        for(int i= 0; i< nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[nonzerotracker],nums[i]);
+                nonzerotracker++;
             }
         }
     }
