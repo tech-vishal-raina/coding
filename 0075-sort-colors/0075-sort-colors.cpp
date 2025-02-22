9a0ea2b9c93caa5f15dae73e590693d57e12d367
tech-vishal-raina <vishuraina0001@gmail.com>
2025-02-22 16:39:49 +0530

    Time: 0 ms (100%), Space: 11.5 MB (66.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // since there are only 0s, 1s and 2s, we can use two pointer appraoch and use 2 for loops
+        int pointer = 0;
+        int cnt = 0;
+        for(int i =0; i<nums.size(); i++){
+            if(nums[i]==0){
+                swap(nums[pointer],nums[i]);
+                cnt++;
+                pointer++;
+            }
+        }
+        pointer = cnt;
+        for(int i=cnt; i<nums.size();i++){
+            if(nums[i]==1){
+                swap(nums[pointer],nums[i]);
+                pointer++;
+            }
+        }
+
     }
 };
